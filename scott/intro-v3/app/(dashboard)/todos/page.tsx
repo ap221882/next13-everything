c4016a9a3c5fdb4cf11cb2e1f3c4a143b4576418
ajay <ap221882@gmail.com>
2023-08-30 20:01:30 +0530

    static routing

---
+++ scott/intro-v3/app/(dashboard)/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const settingsPage = (props: Props) => {
+  return <div>settingsPage</div>
+}
+console.log(process.argv[1])
+
+//~> default export compulsary
+export default settingsPage
